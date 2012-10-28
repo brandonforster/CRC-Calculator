1c89d420adf7deb7dea3c72b2e37ae451f9a39e0
Brandon Forster <brandonforster@gmail.com>
2012-10-27 22:58:49 -0400

    Initial commit.

---
+++ src/crc/CRCConverter.java
@@ -0,0 +1,20 @@
+/*
+ * Brandon Forster
+ * CIS 3360 – Security in Computing Fall 2012
+ * Programming Assignment 2
+ * 7 November 2012
+ */
+
+package crc;
+
+public class CRCConverter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
